041e7800329cf9e7fa80948e5238230748e169e2
omadurav <omadurav@outlook.com>
2023-11-03 08:23:49 -0500

    first commit

---
+++ src/app/dbz/components/add-character/app-add-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-add-character',
+  templateUrl: './app-add-character.component.html',
+  styleUrls: ['./app-add-character.component.css']
+})
+export class AddCharacterComponent {
+
+}
